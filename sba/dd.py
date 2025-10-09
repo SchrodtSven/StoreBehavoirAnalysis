e012c78143093a11cc1ae175a36eace936c52a82
Sven Schrodt <sven@schrodt.club>
2025-10-09 16:11:32 +0200

    Preparing stuff: sanitizing, normalizing data; doq

---
+++ sba/dd.py
@@ -0,0 +1,19 @@
+# Data Dictionary
+#
+# AUTHOR Sven Schrodt
+# SINCE 2025-10-09
+class DD:
+    """ Data Dictionary class matching names<->labels(@GUI) and vice versa
+    """
+    
+    pages = {
+        "Foo": "Test Page",
+        "Home": "Frontpage",
+    }
+
+    col_transl = {
+        
+    }
+
+    
+# cloc --by-file --fullpath --match-d 'dash'
